0c7489183eb7876506f3e14b8a47fe855a5f7583
LeDorime <mohannad2611@gmail.com>
2024-10-02 01:59:35 -0400

    Added neurons, created a model, and calculated the accuracy

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# importing a dataset
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -13,12 +14,15 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=3)
 
-model.save('test.keras')
+loss, accuracy = model.evaluate(x_test, y_test)
+
+print(loss)
+print(accuracy)
